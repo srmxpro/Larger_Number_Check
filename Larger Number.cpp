004b3ea887d79f9e9f4119abb1e40302d3cb807c
Sahidur <83564435+srmxpro@users.noreply.github.com>
2022-09-18 19:07:40 +0530

    Add files via upload

---
+++ Larger Number.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int a,b;
+	
+	printf("Enter first number : ");
+	scanf("%d",&a);
+	printf("Enter second number : ");
+	scanf("%d",&b);
+	
+	if (a>b)
+		printf("%d is larger number",a);
+	else 
+		printf("%d is larger number",b);
+		
+	return 0;
+}
+
+
+
+
